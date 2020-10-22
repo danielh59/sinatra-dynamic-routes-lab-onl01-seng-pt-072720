b8d532580d8022f3d499dd2b7c6d262458db403c
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-22 08:47:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     @x =  @number1.public_send(@operation, @number2 )
-    " #{@number1+ @operation+ @number2}"
+    "#{x}"
   end
 
 end
