b365e6900f7ec455d52d2fd7201dc538c69d2583
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-22 07:57:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name'
+end 
+  get '/square/:number'
+end 
+
+  get '/say/:number/:phrase'
+end 
 
 end
