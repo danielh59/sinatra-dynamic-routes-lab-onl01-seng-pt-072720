9da71cf3132bf84330282afb6de2de74ffa1ce11
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-22 08:39:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    "#{@number1 @number2}"
+    "#{@number1 + @number2}"
   end
 
 end
