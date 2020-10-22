6b7586b569e499ee105c5e664dbfc8bfaa131cb5
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-22 08:04:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name'
-  @reversename = params[:name].reverse 
+  @reversename = params[:name].reverse
   "#{@reversename}"
   end
 
