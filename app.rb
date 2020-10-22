d239575228632553cd687e2a9c146eb880c4502d
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-22 09:04:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    case @operation
+      case @operation
         when "subtract"
           (@number1 - @number2)
         when "add"
@@ -41,7 +41,7 @@ class App < Sinatra::Base
           (@number1 * @number2)
         when "divide"
           (@number1 / @number2)
-        end
+      end
   end
 
 end
